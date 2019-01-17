85833fcde2cf50c37d939648689e47c62c406d11
danil10171 <belaev.d.s@gmail.com>
2019-01-18 01:06:05 +0700

    Add registration

--- quest_answer/app/helpers/application_helper.rb
+++ quest_answer/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def bootstrap_class_for(name)
+    {   sucess: "alert-success",
+        error: "alert-danger",
+        danger: "alert-danger",
+        alert: "alert-warning",
+        notice: "alert-info"
+    }[name.to_sym] || name
+    end
 end
