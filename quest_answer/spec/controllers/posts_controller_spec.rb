f4d367fa14a0605e0936b17c0e364048e5c6de60
dragoblued <qwemee@gmail.com>
2019-01-18 16:33:08 +0700

    change posts_controller_spec

--- quest_answer/spec/controllers/posts_controller_spec.rb
+++ quest_answer/spec/controllers/posts_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe PostsController, type: :controller do
     end
   end
   describe "GEt #show" do
-  	let(:post) {Post.create(title: "Test title", summary: "Test content", body: "Test content")}
+  	let(:post) {Post.create(title: "Test title", summary: "Test summary", body: "Test body")}
 	  it "returns a success response" do
 	  	get :show, params: {id: post}
 	  	expect(response).to be_successful
@@ -17,20 +17,46 @@ RSpec.describe PostsController, type: :controller do
   describe "POST #create valid" do
      it "creates a new Post" do
       expect {
-  			post = Post.create(title: "Test title", summary: "Test content", body: "Test content")
+  			post = Post.create(title: "Test title", summary: "Test summary", body: "Test body")
       }.to change(Post, :count).by(1)
     end
   end
   describe "POST #create invalid" do
      it "creates a new Post (title, body)" do
       expect {
-        post = Post.create(title: "Test title", body: "Test content")
+        post = Post.create(title: "Test title", body: "Test body")
       }.to change(Post, :count).by(0)
     end
+     it "creates a new Post (body, summary)" do
+      expect {
+        post = Post.create(body: "Test body", summary: "Test summary")
+      }.to change(Post, :count).by(0)
+    end
+     it "creates a new Post (title, summary)" do
+      expect {
+        post = Post.create(title: "Test title", summary: "Test summary")
+      }.to change(Post, :count).by(0)
+    end
+      it "creates a new Post (body)" do
+      expect {
+        post = Post.create(body: "Test body")
+      }.to change(Post, :count).by(0)
+    end
+      it "creates a new Post (title)" do
+      expect {
+        post = Post.create(title: "Test title")
+      }.to change(Post, :count).by(0)
+    end
+      it "creates a new Post (summary)" do
+      expect {
+        post = Post.create(summary: "Test summary")
+      }.to change(Post, :count).by(0)
+    end
+
   end
   describe "POST #delete" do
     it "delete a new Post" do
-  	  post = Post.create(title: "Test title", summary: "Test content", body: "Test content")
+  	  post = Post.create(title: "Test title", summary: "Test summary", body: "Test body")
       post.destroy
       expect(Post.count).to eq(0)
     end
